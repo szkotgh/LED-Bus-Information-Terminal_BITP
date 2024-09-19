2619097c31010e2e6dfd956b24ad73a2fb8af59d
suzukaotto <me@szk.kr>
2024-09-19 16:54:39 +0900

    update speaker_manager

--- module/speaker_manager.py
+++ module/speaker_manager.py
@@ -1,10 +1,19 @@
 import os
 import module.utils as utils
-import tempfile
+import requests
 
 class SpeakerManager:
-    def __init__(self):
+    def __init__(self, GOOGLE_KEY, _OPTIONS):
         self.logger = utils.create_logger('speaker_manager')
-        self.tmp_path = tempfile.mkdtemp()
+        self.API_KEY = GOOGLE_KEY
+        self.tmp_path = '/src/audio'
         self.logger.info(f"tmp_path : {self.tmp_path}")
+        
+        self.OPTIONS = _OPTIONS
+        self.API_ERROR_RETRY_COUNT = self.OPTIONS.get('set_api_error_retry_count', 10)
+        self.API_TIMEOUT = self.OPTIONS.get('set_api_timeout', 5)
     
+    def speak_text(self, text):
+        url = "https://texttospeech.googleapis.com/v1beta1/voices"
+        response = requests.get(url)
+        print(response.text)
