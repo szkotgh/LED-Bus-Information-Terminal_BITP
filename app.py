06eac68cb9797fb52aae753d0b68a0563002834e
suzukaotto <me@szk.kr>
2025-01-05 21:17:35 +0900

    update: usrCtrl code stabilty

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ import modules.config as config
 import modules.utils as utils
 import modules.matrix_manager as matrix_manager
 import modules.info_manager as info_manager
-import modules.usrCtrl_manager as usrCtrl_manager
+from modules.userCtrl_manager import get_state, set_states
+import time
 
+while 1:
+    status = get_state()
+    if status is None:
+        continue
+    
+    set_states({"LED1": status['BUTTON1'], "LED2": status['BUTTON2']})
+    time.sleep(0.1)
