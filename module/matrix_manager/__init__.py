4188872f1fc6077683ab4cbd54d08240fbef7ccd
suzukaotto <me@szk.kr>
2025-01-05 10:28:39 +0900

    update: matrix manager struct configurationi complete

--- module/matrix_manager/__init__.py
+++ module/matrix_manager/__init__.py
@@ -2,3 +2,7 @@ import module.config as config
 from PIL import Image, ImageDraw, ImageFont
 
 matrix = config.matrix
+
+def refresh(self, display, status_prt: bool = True):    
+    # display.save('./display.png')k
+    matrix.SetImage(display.convert('RGB'))
