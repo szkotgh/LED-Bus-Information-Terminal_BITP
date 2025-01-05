55e9fd262d8be62d7c443c8ab29a76f280bfdef0
suzukaotto <me@szk.kr>
2025-01-05 20:28:19 +0900

    update: code stabilty

--- modules/usrCtrl_manager/__init__.py
+++ modules/usrCtrl_manager/__init__.py
@@ -8,29 +8,38 @@ SERIAL_BPS = 115200
 arduino = serial.serial_for_url(SERIAL_PORT, SERIAL_BPS, timeout=1)
 time.sleep(2)
 
-def set_device_state(device, state):
-    command = f"{device} : {'true' if state else 'false'}\n"
+def send_command(command):
+    command = f"{json.dumps(command)}\n"
+    print(f"Sending command: {command}", end='')
     arduino.write(command.encode())
     time.sleep(0.05)
+    response = arduino.readline().decode().strip()
+    return json.loads(response) if response else None
 
 def get_device_state(device):
-    command = f"GET : {device}\n"
-    arduino.write(command.encode())
-    time.sleep(0.05)
-    response = arduino.readline().decode().strip()
-    return response
+    command = {"command": "GET", "variable": device}
+    return send_command(command)
 
 def get_all_states():
-    command = "GET : ALL\n"
-    arduino.write(command.encode())
-    time.sleep(0.05)
-    response = arduino.readline().decode().strip()
-    return response
+    command = {"command": "GET_ALL"}
+    return send_command(command)
 
-set_device_state("LED1", True)
-print(get_device_state("LED1"))
+def set_device_state(device, state):
+    command = {"command": "SET", "variable": device, "state": state}
+    return send_command(command)
+
 while True:
-    print(get_all_states())
     time.sleep(1)
-
-time.sleep(100)
+    status = get_all_states()
+    if status == None:
+        print("Failed to get status")
+        continue
+    
+    set_device_state('LED1', status.get('BUTTON1'))
+    set_device_state('LED2', status.get('BUTTON2'))
+    if status.get('LED1')==True or status.get('LED2')==True:
+        set_device_state('LED3', True)
+        set_device_state('LED4', False)
+    else:
+        set_device_state('LED3', False)
+        set_device_state('LED4', True)
