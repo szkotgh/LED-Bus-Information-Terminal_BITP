598d5f463718a93b0bc92181d2908fe109690ac8
suzukaotto <me@szk.kr>
2025-01-13 21:16:52 +0900

    update: code stabilty

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import modules.matrix_manager as matrix_manager
-import modules.info_manager as info_manager
+import modules.info_manager as info_manage
+import modules.info_manager.apis.control_pannel as control_pannel
 matrix = matrix_manager
 
 while True:
     # matrix.matrix_pages.bus_station_etc_page(0, 10)
-    print(info_manager.service.control_pannel.status_devices)
     matrix.matrix_pages.start_page(10)
+    print(control_pannel.get_state())
+    control_pannel.set_states({"LED1": True, "LED2": False, "LED3": False, "LED4": False})
+    control_pannel.set_states({"LED1": False, "LED2": True, "LED3": False, "LED4": False})
+    control_pannel.set_states({"LED1": False, "LED2": False, "LED3": True, "LED4": False})
+    control_pannel.set_states({"LED1": False, "LED2": False, "LED3": False, "LED4": True})
+    
+    control_pannel.set_states({"RELAY1": True, "RELAY2": True, "RELAY3": True, "RELAY4": True})
+    control_pannel.set_states({"RELAY1": False, "RELAY2": False, "RELAY3": False, "RELAY4": False})
--- modules/info_manager/__init__.py
+++ modules/info_manager/__init__.py
@@ -5,26 +5,19 @@ import modules.info_manager.apis.bus as bus
 import modules.info_manager.apis.weather as weather
 import modules.info_manager.apis.everline as everline
 import modules.info_manager.apis.network as network
-import modules.info_manager.apis.control_pannel as control_pannel
+# import modules.info_manager.apis.control_pannel as control_pannel
 
 class InfoManager:
     def __init__(self, SERVICE_KEY):
-        print('InfoManager Init')
-        print('init everline')
         self.everline = everline
         self.everline_api = self.everline.EverlineAPI(_req_url=config.OPTIONS['everline']['reqUrl'])
         self.everline_api.start_auto_update(_interval=config.OPTIONS['everline']['refreshInterval'])
-        print('inited everline')
         
-        print('init network')
         self.network = network.NetworkManager(_req_url=config.OPTIONS['network']['reqUrl'], _timeout=config.OPTIONS['api_timeout'])
         self.network.start_auto_update(_interval=config.OPTIONS['network']['refreshInterval'])
-        print('inited network')
         
-        print('init control_pannel')
-        self.control_pannel = control_pannel.control_pannel
-        self.control_pannel = self.control_pannel.auto_refresh(_interval_sec=config.OPTIONS['control_pannel']['refreshInterval'])
-        print('inited control_pannel')
+        # self.control_pannel = control_pannel.ControlPannel(_arduino_port=config.OPTIONS['control_pannel']['serialPort'], _arduino_bps=config.OPTIONS['control_pannel']['serialBps'])
+        # self.control_pannel = self.control_pannel.auto_refresh(_interval_sec=config.OPTIONS['control_pannel']['refreshInterval'])
         
         with open('./station_datas_struct.json', 'r') as f:
             self.station_datas = json.load(f)
